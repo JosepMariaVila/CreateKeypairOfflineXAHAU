4bc381c65437943b24fcd11f1be4ef753c70f52e
Josep Maria Vila <156566553+JosepMariaVila@users.noreply.github.com>
2024-12-07 18:28:33 +0100

    Add files via upload

---
+++ xahauwallet.js
@@ -0,0 +1,27 @@
+// ******************************************************
+// ************* Get the Preferred Network **************
+// ******************************************************
+
+function getNet() {
+  let net;
+  if (document.getElementById("mn").checked) net = "wss://xahau.network";
+  return net;
+} // End of getNet()
+
+// *******************************************************
+// ************* Create XAHAU Mainnet Account ************
+// *******************************************************
+async function getAccount2() {
+  const wallet = xrpl.Wallet.generate("ed25519");
+  console.log(wallet);
+
+  standbyResultField.value =
+    "Creating a wallet...\n" + "Wallet created:\n" + JSON.stringify(wallet);
+
+  standbyAccountField.value = wallet.address;
+  standbySeedField.value = wallet.seed;
+} // End of getAccount2()
+
+async function reload() {
+  window.location.reload();
+}
